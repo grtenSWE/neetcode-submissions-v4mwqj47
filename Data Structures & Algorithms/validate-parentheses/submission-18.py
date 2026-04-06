572b2b1f024990472301283a9301db7e718a491d
grtenSWE <152864953+grtenSWE@users.noreply.github.com>
2026-04-06 20:25:06 +1200

    Add: validate-parentheses - submission-18

---
+++ Data Structures & Algorithms/validate-parentheses/submission-18.py
@@ -0,0 +1,26 @@
+#best solution
+
+#use a hashmap to map the closing to the opening
+#so we can check if the value at the index matches with the stack pop
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        pairMap = {')':'(', '}':'{', ']':'['}
+
+        for bracket in s:
+            if bracket in pairMap: #if its a closing bracket
+                if stack and stack[-1] == pairMap[bracket]:
+                    stack.pop()
+                else:
+                    return False
+        
+                
+            else:
+                stack.append(bracket)
+
+        return True if not stack else False
+
+
+        
