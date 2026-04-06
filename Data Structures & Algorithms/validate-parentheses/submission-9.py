eaed4a71c2ee34eeb4124183ade87441b4a43adc
grtenSWE <152864953+grtenSWE@users.noreply.github.com>
2026-04-06 17:58:45 +1200

    Add: validate-parentheses - submission-9

---
+++ Data Structures & Algorithms/validate-parentheses/submission-9.py
@@ -0,0 +1,39 @@
+#first and last index are of the same bracket type.
+#slides into the middle
+#only even length is valid
+
+#how to compare two brackets?
+# - 
+
+#Steps to solve a leetcode problem
+
+#understand
+#consider all (many) test cases
+#come up with solution, or bruteforce to start of with
+#write down solution on paper/laptop
+#write solution
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        openBrackets = ['(','[','{']
+        completeBrackets = ['()','[]','{}']
+        stack = []
+
+        if len(s) % 2 != 0:
+            return False
+        
+        for bracket in s:
+            if bracket in openBrackets:
+                stack.append(bracket)
+            else: 
+                if len(stack) == 0:
+                    return False
+                openBracket = stack.pop()
+                if openBracket + bracket not in completeBrackets:
+                    return False
+
+       
+        return True if not stack else False
+
+
+        
