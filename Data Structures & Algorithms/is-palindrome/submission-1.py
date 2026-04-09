28e6018c220dd80a9fa521cfcd3a6dbaf156c7c1
grtenSWE <152864953+grtenSWE@users.noreply.github.com>
2026-04-09 19:21:13 +1200

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,24 @@
+#keep a left and right pointer.
+#if both of them are alphanum then compare them
+#if one of them is not then shift and then check again
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l = 0
+        r = len(s) - 1
+
+        while l < r:
+            if not s[l].isalnum():
+                l += 1
+            elif not s[r].isalnum():
+                r -= 1
+            elif s[l].lower() != s[r].lower():
+                return False
+            else:
+                l += 1
+                r -= 1
+
+        return True
+
+        
