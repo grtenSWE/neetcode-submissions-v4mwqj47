b989a616c4418ba95116e9679806c1cf5964fa33
grtenSWE <152864953+grtenSWE@users.noreply.github.com>
2026-04-11 18:10:21 +1200

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,20 @@
+#trying to imporve for space complexity
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        sCount = [0]*26
+
+
+        if len(s) != len(t):
+            return False
+
+        for c in s:
+            sCount[ord(c) - ord('a')] += 1
+
+        for c in t:
+            sCount[ord(c) - ord('a')] -= 1
+            if sCount[ord(c) - ord('a')] < 0:
+                return False
+
+ 
+        return True
