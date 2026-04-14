728a3ef5117078b84d5223cabbb99ce9c5bbf216
grtenSWE <152864953+grtenSWE@users.noreply.github.com>
2026-04-14 17:15:15 +1200

    Add: combination-target-sum - submission-3

---
+++ Data Structures & Algorithms/combination-target-sum/submission-3.py
@@ -0,0 +1,42 @@
+#recursive solution.
+
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        #sort nums once
+
+        nums.sort()
+        res = []
+
+        #pass through the solution to get it's count for each number
+        #check if this exact count is already in the List
+
+        combs = [] #dict of dict
+
+        def combSum(target, currentSol):
+            #base case
+            if target == 0:
+                print(currentSol) 
+                #we need to also check if this is a unique combination
+                
+                res.append(currentSol[1:])
+
+
+            for num in nums:
+                #print(currentSol)
+                #print(num)
+                #print("____")
+                if num <= target and currentSol[-1] <= num:
+                    curr = currentSol.copy()
+                    curr.append(num)
+                    combSum(target-num, curr)
+                elif num > target:
+                    break
+                
+
+        combSum(target, [0])
+        return res
+
+                    
+        
+    
+        
