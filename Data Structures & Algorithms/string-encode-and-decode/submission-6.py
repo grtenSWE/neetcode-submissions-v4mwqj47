ab62a54d7180a75f8c3c832d2114b3617e8cde73
grtenSWE <152864953+grtenSWE@users.noreply.github.com>
2026-04-14 14:22:01 +1200

    Add: string-encode-and-decode - submission-6

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-6.py
@@ -0,0 +1,44 @@
+#save the index to separate the str at the end of the string
+#solved in 70 minutes
+
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        if not strs:
+            return "''"
+
+        encode = ""
+        sepList = "["
+
+        idx = - 1
+
+        for string in strs[:-1]:
+            idx = idx + len(string) + 1 
+            sepList = sepList + " " + str(idx)
+            encode = encode + " " + string 
+
+        encode = encode + " " + strs[-1] + str(sepList)
+        return encode[1:]
+
+    def decode(self, s: str) -> List[str]:
+        if s == "''":
+            return []
+
+        sepList = ""
+        i = len(s) - 1
+        while s[i] != '[':
+            sepList = s[i] + sepList 
+            i -= 1
+
+        s = s[:i]
+        last_i = -1
+        decode = []
+        for i in sepList.split():
+            i = int(i)
+            decode.append(s[last_i+1:i])
+            last_i = i
+
+        decode.append(s[last_i+1:])
+        
+        return decode
+
