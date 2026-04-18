47a7f9f5fc6dc8d79b62cd202a5708e27d37124d
grtenSWE <152864953+grtenSWE@users.noreply.github.com>
2026-04-18 13:11:48 +1200

    Add: level-order-traversal-of-binary-tree - submission-4

---
+++ Data Structures & Algorithms/level-order-traversal-of-binary-tree/submission-4.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        #base case
+        if not root:
+            return []
+
+        leftOrder = self.levelOrder(root.left)
+        rightOrder = self.levelOrder(root.right)
+        comb = [[root.val]]
+
+        #run the combination algo for len of smaller list
+
+        for i in range(min(len(leftOrder),len(rightOrder))):
+            comb.append(leftOrder[i] + rightOrder[i])
+
+        #then add remaining items from larger list
+
+        if len(leftOrder) < len(rightOrder):
+            for i in range(len(leftOrder),len(rightOrder)):
+                comb.append(rightOrder[i])
+        else:
+            for i in range(len(rightOrder), len(leftOrder)):
+                comb.append(leftOrder[i])
+     
+                
+
+
+        return comb
